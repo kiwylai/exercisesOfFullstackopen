b1d441ad034a503104250cb3f67aa9e2818b04d4
kiwylai <kiwylai@gmail.com>
2025-05-27 12:22:06 +0200

    2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
 const handleWith = (handler)=>{ 
   return (event) => {
@@ -42,13 +43,19 @@ const Persons = ({ persons,searchTerm }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   return (
     <div>     
       <h2>Phonebook</h2>
