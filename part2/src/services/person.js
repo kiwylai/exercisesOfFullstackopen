c3b6d0d2e692dabfb6682afe39be96594129c5c1
kiwylai <kiwylai@gmail.com>
2025-05-28 15:08:14 +0200

    add a new function

--- part2/src/services/person.js
+++ part2/src/services/person.js
@@ -12,6 +12,11 @@ const create = newObject => {
   return request.then(response => response.data)
 }
 
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
@@ -20,5 +25,6 @@ const update = (id, newObject) => {
 export default { 
   getAll, 
   create, 
+  remove,
   update 
 }
