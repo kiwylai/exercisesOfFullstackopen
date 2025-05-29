a716cb0770aca9f181948ff5043404676361b7df
kiwylai <kiwylai@gmail.com>
2025-05-29 11:39:59 +0200

    create Footer.jsx, add Notification component, add css

--- notes project/notes/src/App.jsx
+++ notes project/notes/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Footer from './components/Footer'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  if ()
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -20,12 +35,15 @@ const App = () => {
     const changedNote = { ...note, important: !note.important }
     noteService
       .update(id, changedNote)
-       .then(returnedNote => {
+      .then(returnedNote => {
         setNotes(notes.map(note => note.id === id ? returnedNote : note))
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
     })
     })
@@ -51,11 +69,10 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
-
-
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -70,6 +87,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
