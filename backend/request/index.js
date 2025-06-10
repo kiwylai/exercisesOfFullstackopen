d40940a72a115e3f345ac2c6e464b4f9f349e110
kiwylai <kiwylai@gmail.com>
2025-06-10 12:41:21 +0200

    delete duplicate PORT

--- backend/request/index.js
+++ backend/request/index.js
@@ -2,10 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+
 
 app.use(cors())
 
