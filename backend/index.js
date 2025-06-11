f756a6819b15206a2611fda256681831d3eac961
kiwylai <kiwylai@gmail.com>
2025-06-11 12:29:23 +0200

    build the frontedn

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const registerRoutesForNotesIn = require('./notesBackend')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :type`))
 
