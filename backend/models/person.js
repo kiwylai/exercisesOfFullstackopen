18a363438f4f8f22e52f6d030de4a77b662ef315
kiwylai <kiwylai@gmail.com>
2025-06-18 13:01:05 +0200

    complete 3.20

--- backend/models/person.js
+++ backend/models/person.js
@@ -8,8 +8,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 8,
-    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
   },
 });
 
