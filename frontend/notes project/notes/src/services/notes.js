1d08347b3d8e873e78d4c2000c1ab4a7d01486e7
kiwylai <kiwylai@gmail.com>
2025-06-18 12:50:46 +0200

    update frontend 3.20

--- frontend/notes project/notes/src/services/notes.js
+++ frontend/notes project/notes/src/services/notes.js
@@ -1,24 +1,35 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
+import axios from "axios";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-export default { 
-  getAll, 
-  create, 
-  update 
-}
-
+export default {
+  getAll,
+  create,
+  update,
+};
