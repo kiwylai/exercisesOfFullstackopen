ac6f40b4ce1d8b7f8932f5271c8c325fc9b22fbc
kiwylai <kiwylai@gmail.com>
2025-07-04 10:23:35 +0200

    update

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/note_user')
 
 notesRouter.get('/', (request, response) => {
   Note.find({}).then(notes => {
@@ -18,13 +19,20 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
